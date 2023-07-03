d2b1a02f808cbb19a258859ca8f99a8a491f4ca8
kai <cgk0910@gmail.com>
2023-07-03 11:41:46 +0800

    建立常用的函式在base.php

--- base.php
+++ base.php
@@ -3,9 +3,20 @@
 $BASEDIR=__DIR__;
 session_start();
 
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
 
+function to($url){
+    header("location:".$url);
+}
 
-
+function q($sql){
+    $pdo=new PDO("mysql:host=localhost;charset=utf8;dbname=db13",'root','');
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
 
 
 
